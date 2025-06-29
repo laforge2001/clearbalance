4fb5035f4e4372ea13a1f49d690277f884c95f3a
laforge2001 <laforge2001@gmail.com>
2025-06-28 22:53:58 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+// const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// )
 
 export default function Home() {
   useEffect(() => {
